3dc03a429615ab437b43011c3c20092c13348890
buildYourCV <abhishek3feb1991@gmail.com>
2025-08-23 04:03:04 +0530

    bcv

--- src/App.jsx
+++ src/App.jsx
@@ -29,6 +29,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p> */}
+      <h1>Welcome !!</h1>
       <BuildCV />
     </>
   )
